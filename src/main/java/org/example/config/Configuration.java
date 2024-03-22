2685f017ce9fc381d3ee185a9528c63594d0343c
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-03-22 13:12:38 +0100

    added constructor to conf

--- src/main/java/org/example/config/Configuration.java
+++ src/main/java/org/example/config/Configuration.java
@@ -1,7 +1,10 @@
 package org.example.config;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.aeonbits.owner.ConfigFactory;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Configuration {
 
     public static final ServerConfig CONFIG;
